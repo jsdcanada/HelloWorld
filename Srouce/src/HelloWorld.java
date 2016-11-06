e2f9235e05275d7f2ceb1d5f31ab00788fd38877
jsdcanada <jagdishchandra@gmail.com>
2016-11-06 16:11:09 +0000

    Added Exception Handling

--- Srouce/src/HelloWorld.java
+++ Srouce/src/HelloWorld.java
@@ -3,6 +3,39 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println(Divide(10,0));
         System.out.println("This is testing for github");
+    }
+
+    public static int Divide(int a, int b)
+    {
+        try
+        {
+            return  a/b;
+        }
+        catch (ArithmeticException ae)
+        {
+            System.out.println("Encountered ArithmeticException");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Handled by Exception");
+        }
+        catch (Throwable te)
+        {
+            System.out.println("Throwable");
+        }
+        finally {
+            System.out.println("Divide operation completed");
+        }
+        return -1;
+    }
+
+    public class Calculator
+    {
+        public double Divide (double a, double b)
+        {
+            return a/b;
+        }
     }
 }
