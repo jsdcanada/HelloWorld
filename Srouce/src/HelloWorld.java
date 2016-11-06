c93a5ad6f3d667f6e8548fe6ab6a7fdf88b75ea0
jsdcanada <jagdishchandra@gmail.com>
2016-11-06 15:21:19 +0000

    Adding HelloWorld.java

---
+++ Srouce/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by E7440 on 06/11/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is testing for github");
+    }
+}
